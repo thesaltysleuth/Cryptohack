d4b70fca3e553124649fbf205c097645caffdc2b
Saswata Som <50485488+thesaltysleuth@users.noreply.github.com>
2020-06-15 20:32:09 +0530

    Add files via upload

---
+++ nth_root.py
@@ -0,0 +1,24 @@
+def nth_root(x, n):
+    # Start with some reasonable bounds around the nth root.
+    upper_bound = 1
+    while upper_bound ** n <= x:
+        upper_bound *= 2
+    lower_bound = upper_bound // 2
+    # Keep searching for a better result as long as the bounds make sense.
+    while lower_bound < upper_bound:
+        mid = (lower_bound + upper_bound) // 2
+        mid_nth = mid ** n
+        if lower_bound < mid and mid_nth < x:
+            lower_bound = mid
+        elif upper_bound > mid and mid_nth > x:
+            upper_bound = mid
+        else:
+            # Found perfect nth root.
+            return mid
+    return mid + 1
+
+
+
+#x = 2 ** 100
+#cube = x ** 3
+#root = nth_root(cube, 3)
